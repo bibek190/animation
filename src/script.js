de4ab934f3ec9a6c527b952140162632ea4fd0d4
bibek190 <bibek9818917036@gmail.com>
2024-09-24 13:43:41 +1000

    annimation basics

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import gsap from "gsap";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -29,15 +30,20 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 
-const clock = new THREE.Clock();
+// const clock = new THREE.Clock();
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: -2 });
+gsap.to(mesh.position, { duration: 1, delay: 3, x: 0 });
 
 const tick = () => {
   // clock
-  const elapsedTime = clock.getElapsedTime();
+  //   const elapsedTime = clock.getElapsedTime();
 
-  camera.position.y = Math.sin(elapsedTime);
-  camera.position.x = Math.cos(elapsedTime);
+  //   camera.position.y = Math.sin(elapsedTime);
+  //   camera.position.x = Math.cos(elapsedTime);
   //   camera.lookAt(mesh.position);
+  mesh.rotation.z += 0.01;
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
